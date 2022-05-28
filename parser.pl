4f06c26c9bb07bb7ab0c4f6dfadc4190fdb516e1
claw5ie <92851066+claw5ie@users.noreply.github.com>
2022-05-28 20:39:02 +0100

    Complete the parser

--- parser.pl
+++ parser.pl
@@ -1,6 +1,139 @@
 :- module(parser, [parse/2]).
 
-parse(String, Ast) :- tokenize(String, Tokens), parse(Tokens, [end_of_file], Ast).
+parse(String, Ast) :-
+    tokenize(String, Tokens),
+    parse_top_level(Tokens, [], Ast).
+
+parse_top_level([defun | Tokens], Rest, [Defun | Ast]) :-
+    parse_defun(Tokens, Tokens0, Defun),
+    parse_top_level(Tokens0, Rest, Ast).
+
+parse_top_level([identifier(Id) | Tokens], Rest, [VarDef | Ast]) :-
+    parse_vardef([identifier(Id) | Tokens], Tokens1, VarDef),
+    parse_top_level(Tokens1, Rest, Ast).
+
+parse_top_level([end_of_file], [], []).
+
+parse_expr(Tokens, Rest, Expr) :-
+    parse_level(0, Tokens, Rest, Expr).
+
+parse_level(Level, Tokens, Rest, Ast) :-
+    Level < 6,
+    NextLevel is Level + 1,
+    parse_level(NextLevel, Tokens, Tokens0, Left),
+    fold_exprs(Level, Tokens0, Left, Rest, Ast).
+
+parse_level(6, [lparen | Tokens], Rest, Ast) :-
+    parse_expr(Tokens, [rparen | Rest], Ast).
+
+parse_level(6, [minus | Tokens], Rest, apply(minus, Ast)) :-
+    parse_expr(Tokens, Rest, Ast).
+
+parse_level(6, [integer(Int) | Rest], Rest, integer(Int)).
+
+parse_level(
+    6, [identifier(Id), lparen | Tokens], Rest, fun_call(Id, Args)
+) :-
+    parse_expr_list(Tokens, [rparen | Rest], Args).
+
+parse_level(6, [identifier(Id) | Rest], Rest, identifier(Id)).
+
+fold_exprs(Level, [Op | Tokens], Left, Rest, Result) :-
+    op_prec(Op, Level),
+    !,
+    NextLevel is Level + 1,
+    parse_level(NextLevel, Tokens, Tokens0, Right),
+    fold_exprs(Level, Tokens0, apply(Op, Left, Right), Rest, Result).
+
+fold_exprs(_, Rest, Left, Rest, Left).
+
+parse_expr_list(Tokens, Rest, [Expr | ExprList]) :-
+    parse_expr(Tokens, Tokens0, Expr),
+    parse_rest_expr_list(Tokens0, Rest, ExprList).
+
+parse_rest_expr_list([comma | Tokens], Rest, ExprList) :-
+    parse_expr_list(Tokens, Rest, ExprList).
+
+parse_rest_expr_list(Rest, Rest, []).
+
+parse_body([defun | Tokens], Rest, [FunDef | Statements]) :-
+    parse_defun(Tokens, Tokens0, FunDef),
+    parse_body(Tokens0, Rest, Statements).
+
+parse_body([return | Tokens], Rest, [Expr | Body]) :-
+    parse_expr(Tokens, [semicolon | Tokens0], Expr),
+    fold_body(Tokens0, Rest, Body).
+
+parse_body([if | Tokens], Rest, [If | Body]) :-
+    parse_if(Tokens, Tokens0, If),
+    parse_body(Tokens0, Rest, Body).
+
+parse_body([while | Tokens], Rest, [While | Body]) :-
+    parse_while(Tokens, Tokens0, While),
+    parse_body(Tokens0, Rest, Body).
+
+parse_body([else | Rest], [else | Rest], []).
+
+parse_body([semicolon | Rest], Rest, []).
+
+parse_body(Tokens, Rest, [VarDef | Body]) :-
+    parse_vardef(Tokens, Tokens1, VarDef),
+    parse_body(Tokens1, Rest, Body).
+
+parse_body(Tokens, Rest, [Expr | Body]) :-
+    parse_expr(Tokens, [semicolon | Tokens1], Expr),
+    parse_body(Tokens1, Rest, Body).
+
+parse_vardef(
+    [identifier(Id), colon, Type | Tokens], Rest, vardef(Type, Id, Expr)
+) :-
+    type_is_non_void(Type),
+    parse_rest_vardef(Tokens, Rest, Expr).
+
+parse_rest_vardef([semicolon | Rest], Rest, uninitialized).
+
+parse_rest_vardef([equal | Tokens], Rest, Expr) :-
+    parse_expr(Tokens, [semicolon | Rest], Expr).
+
+parse_defun(
+    [identifier(Id), lparen | Tokens], Rest, defun(Type, Id, Args, Body)
+) :-
+    parse_param_list(Tokens, [rparen, colon, Type | Tokens0], Args),
+    is_valid_type(Type),
+    parse_body(Tokens0, Rest, Body).
+
+parse_param_list(
+    [identifier(Id), colon, Type | Tokens], Rest, [param(Type, Id) | Params]
+) :-
+    type_is_non_void(Type),
+    parse_rest_param_list(Tokens, Rest, Params).
+
+parse_param_list(Rest, Rest, []).
+
+parse_rest_param_list([comma | Tokens], Rest, Params) :-
+    parse_param_list(Tokens, Rest, Params).
+
+parse_rest_param_list(Rest, Rest, []).
+
+parse_if(Tokens, Rest, if(Cond, IfTrue, IfFalse)) :-
+    parse_expr(Tokens, [colon | Tokens1], Cond),
+    parse_body(Tokens1, Tokens2, IfTrue),
+    parse_else(Tokens2, Rest, IfFalse).
+
+parse_else([else | Tokens], Rest, Else) :-
+    parse_body(Tokens, Rest, Else).
+
+parse_else(Rest, Rest, []).
+
+parse_while(Tokens, Rest, while(Cond, Body)) :-
+    parse_expr(Tokens, [colon | Tokens1], Cond),
+    parse_body(Tokens1, Rest, Body).
+
+type_is_non_void(Type) :- Type \= void_type, is_valid_type(Type).
+
+is_valid_type(int_type).
+is_valid_type(string_type).
+is_valid_type(void_type).
 
 op_prec(or, 0).
 
@@ -20,29 +153,3 @@ op_prec(minus, 4).
 op_prec(multiply, 5).
 op_prec(divide, 5).
 op_prec(modulus, 5).
-
-op_prec(negation, 6).
-
-parse(Tokens, Rest, Ast) :- parse_level(0, Tokens, Rest, Ast).
-
-parse_level(Level, Tokens, Rest, Ast) :-
-    Level < 7,
-    !,
-    NextLevel is Level + 1,
-    parse_level(NextLevel, Tokens, [Op | Tokens0], Left),
-    (op_prec(Op, Level) ->
-         parse_level(Level, Tokens0, Rest, Right),
-         Ast = apply(Op, Left, Right) ;
-     Rest = [Op | Tokens0], Ast = Left).
-
-parse_level(_, [lparen | Tokens], Rest, Ast) :-
-    parse_level(0, Tokens, [rparen | Rest], Ast).
-
-parse_level(_, [minus | Tokens], Rest, apply(minus, Ast)) :-
-    parse_level(0, Tokens, Rest, Ast).
-
-parse_level(_, [integer(Int) | Rest], Rest, Int).
-
-%% Should parse function call here.
-
-parse_level(_, [identifier(Id) | Rest], Rest, Id).
