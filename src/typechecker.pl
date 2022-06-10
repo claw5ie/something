4ed4f48ec366eba2187f63bb57f68d406dfc0b6a
claw5ie <92851066+claw5ie@users.noreply.github.com>
2022-06-10 10:40:44 +0100

    Refactor typechecker

--- src/typechecker.pl
+++ src/typechecker.pl
@@ -1,120 +1,115 @@
 :- module(typechecker, [typecheck/1]).
 
 typecheck(Ast) :-
-    list_to_assoc([], Enviroment),
+    list_to_assoc([], Env),
     typecheck_statements(
-        flags(not_in_fun, not_in_loop, void_type), Enviroment, Ast
+        Ast, info(Env, flags(not_in_fun, not_in_loop, void_type))
     ).
 
-typecheck(
-    flags(IsInFun, IsInLoop, ReturnType),
-    Enviroment,
-    while(Cond, Body),
-    void_type
-) :-
-    typecheck(flags(IsInFun, IsInLoop, ReturnType), Enviroment, Cond, bool_type),
-    typecheck_statements(flags(IsInFun, in_loop, ReturnType), Enviroment, Body).
+typecheck(while(Cond, Body), Info, void_type) :-
+    typecheck(Cond, Info, bool_type),
+    Info = info(Env, flags(IsInFun, _, ReturnType)),
+    typecheck_statements(
+        Body, info(Env, flags(IsInFun, in_loop, ReturnType))
+    ).
 
-typecheck(Flags, Enviroment, if(Cond, IfTrue, IfFalse), void_type) :-
-    typecheck(Flags, Enviroment, Cond, bool_type),
-    typecheck_statements(Flags, Enviroment, IfTrue),
-    typecheck_statements(Flags, Enviroment, IfFalse).
+typecheck(if(Cond, IfTrue, IfFalse), Info, void_type) :-
+    typecheck(Cond, Info, bool_type),
+    typecheck_statements(IfTrue, Info),
+    typecheck_statements(IfFalse, Info).
 
-typecheck(Flags, Enviroment, expr(Expr), Type) :-
-    typecheck(Flags, Enviroment, Expr, Type),
+typecheck(expr(Expr), Info, Type) :-
+    typecheck(Expr, Info, Type),
     is_non_void(Type).
 
-typecheck(
-    flags(in_fun, _, void_type),
-    _,
-    return(),
-    void_type
-).
+typecheck(return(), info(_, flags(in_fun, _, void_type)), void_type).
 
-typecheck(
-    flags(in_fun, IsInLoop, ReturnType),
-    Enviroment,
-    return(Expr),
-    void_type
-) :-
-    typecheck(flags(in_fun, IsInLoop, ReturnType), Enviroment, Expr, ReturnType).
+typecheck(return(Expr), Info, void_type) :-
+    Info = info(_, flags(in_fun, _, ReturnType)),
+    typecheck(Expr, Info, ReturnType).
 
-typecheck(flags(_, in_loop, _), _, break, void_type).
+typecheck(break, info(_, flags(_, in_loop, _)), void_type).
 
-typecheck(_, Enviroment, id(Id), Type) :-
-    get_assoc(Id, Enviroment, var_type(Type)).
+typecheck(id(Id), info(Env, _), Type) :-
+    get_assoc(Id, Env, var_type(Type)).
 
-typecheck(Flags, Enviroment, apply(Op, Left, Right), ExprType) :-
-    typecheck(Flags, Enviroment, Left, OperandType),
-    typecheck(Flags, Enviroment, Right, OperandType),
+typecheck(apply(Op, Left, Right), Info, ExprType) :-
+    typecheck(Left, Info, OperandType),
+    typecheck(Right, Info, OperandType),
     infere_expr_type(Op, OperandType, ExprType).
 
-typecheck(Flags, Enviroment, funcall(Id, Args), ReturnType) :-
-    get_assoc(Id, Enviroment, fun_type(ReturnType, ParamTypes)),
-    check_arg_types(pair(Flags, Enviroment), Args, ParamTypes).
+typecheck(funcall(Id, Args), Info, ReturnType) :-
+    Info = info(Env, _),
+    get_assoc(Id, Env, fun_type(ReturnType, ParamTypes)),
+    check_arg_types(Info, Args, ParamTypes).
 
-typecheck(_, _, int(_), int_type).
+typecheck(int(_), _, int_type).
 
-typecheck(_, _, bool(_), bool_type).
+typecheck(bool(_), _, bool_type).
 
-typecheck(Flags, Enviroment, apply(_, Expr), int_type) :-
-    typecheck(Flags, Enviroment, Expr, int_type).
+typecheck(apply(_, Expr), Info, int_type) :-
+    typecheck(Expr, Info, int_type).
 
-typecheck(Flags, Enviroment, inline_if(Cond, IfTrue, IfFalse), Type) :-
-    typecheck(Flags, Enviroment, Cond, bool_type),
-    typecheck(Flags, Enviroment, IfTrue, Type),
-    typecheck(Flags, Enviroment, IfFalse, Type).
+typecheck(inline_if(Cond, IfTrue, IfFalse), Info, Type) :-
+    typecheck(Cond, Info, bool_type),
+    typecheck(IfTrue, Info, Type),
+    typecheck(IfFalse, Info, Type).
 
-typecheck(Flags, Enviroment, assign(Id, Expr), void_type) :-
-    get_assoc(Id, Enviroment, var_type(Type)),
-    typecheck(Flags, Enviroment, Expr, Type).
+typecheck(assign(Id, Expr), Info, void_type) :-
+    Info = info(Env, _),
+    get_assoc(Id, Env, var_type(Type)),
+    typecheck(Expr, Info, Type).
 
-typecheck_statements(Flags, Enviroment, [Statement | Body]) :-
-    add_statement_to_enviroment(Flags, Enviroment, Statement, NewEnviroment),
-    typecheck_statements(Flags, NewEnviroment, Body).
+typecheck_statements([Statement | Body], Info) :-
+    add_statement_to_enviroment(Statement, Info, NewInfo),
+    typecheck_statements(Body, NewInfo).
 
-typecheck_statements(_, _, []).
-
-add_statement_to_enviroment(_, Enviroment, vardef(Type, Id), NewEnviroment) :-
-    put_assoc(Id, Enviroment, var_type(Type), NewEnviroment).
+typecheck_statements([], _).
 
 add_statement_to_enviroment(
-    Flags,
-    Enviroment,
-    vardef(Type, Id, Expr),
-    NewEnviroment
+    vardef(Type, Id),
+    info(Env, Flags),
+    info(NewEnv, Flags)
 ) :-
-    typecheck(Flags, Enviroment, Expr, Type),
-    put_assoc(Id, Enviroment, var_type(Type), NewEnviroment).
+    put_assoc(Id, Env, var_type(Type), NewEnv).
 
 add_statement_to_enviroment(
-    _,
-    Enviroment,
+    vardef(Type, Id, Expr),
+    info(Env, Flags),
+    info(NewEnv, Flags)
+) :-
+    typecheck(Expr, info(Env, Flags), Type),
+    put_assoc(Id, Env, var_type(Type), NewEnv).
+
+add_statement_to_enviroment(
     defun(Type, Id, Params, Body),
-    EnvFunType
+    info(Env, Flags),
+    info(EnvFunType, Flags)
 ) :-
     put_defun_in_map(
-        defun(Type, Id, Params), Enviroment, pair(EnvFunType, EnvParamTypes)
+        defun(Type, Id, Params), Env, pair(EnvFunType, EnvParamTypes)
     ),
-    typecheck_statements(flags(in_fun, not_in_loop, Type), EnvParamTypes, Body).
+    typecheck_statements(
+        Body, info(EnvParamTypes, flags(in_fun, not_in_loop, Type))
+    ).
 
-add_statement_to_enviroment(Flags, Enviroment, Statement, Enviroment) :-
-    typecheck(Flags, Enviroment, Statement, _).
+add_statement_to_enviroment(Statement, Info, Info) :-
+    typecheck(Statement, Info, _).
 
 put_defun_in_map(
     defun(ReturnType, Id, Params),
-    Enviroment,
+    Env,
     pair(EnvFunType, EnvParamTypes)
 ) :-
     extract_types_of_params(Params, ParamTypes),
-    put_assoc(Id, Enviroment, fun_type(ReturnType, ParamTypes), EnvFunType),
+    put_assoc(Id, Env, fun_type(ReturnType, ParamTypes), EnvFunType),
     put_params_in_map(Params, EnvFunType, EnvParamTypes).
 
-put_params_in_map([param(Type, Id) | Params], Enviroment, NewEnviroment) :-
-    put_assoc(Id, Enviroment, var_type(Type), Enviroment0),
-    put_params_in_map(Params, Enviroment0, NewEnviroment).
+put_params_in_map([param(Type, Id) | Params], Env, NewEnv) :-
+    put_assoc(Id, Env, var_type(Type), Env0),
+    put_params_in_map(Params, Env0, NewEnv).
 
-put_params_in_map([], Enviroment, Enviroment).
+put_params_in_map([], Env, Env).
 
 extract_types_of_params([param(Type, _) | Params], [Type | Types]) :-
     extract_types_of_params(Params, Types).
@@ -122,12 +117,12 @@ extract_types_of_params([param(Type, _) | Params], [Type | Types]) :-
 extract_types_of_params([], []).
 
 check_arg_types(
-    pair(Flags, Enviroment),
+    Info,
     [Expr | Args],
     [ExpectedType | ParamTypes]
 ) :-
-    typecheck(Flags, Enviroment, Expr, ExpectedType),
-    check_arg_types(pair(Flags, Enviroment), Args, ParamTypes).
+    typecheck(Expr, Info, ExpectedType),
+    check_arg_types(Info, Args, ParamTypes).
 
 check_arg_types(_, [], []).
 
